72f5d19b211f6b34ad749a2be5bfbf0f996837a6
chandhru216 <kc3936650@gmail.com>
2026-07-06 23:12:47 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 ADD THIS LINE RIGHT HERE
   server: {
     port: 3000,
     allowedHosts: true,
